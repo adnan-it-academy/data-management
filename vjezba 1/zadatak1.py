2ea614dfeb9cbcf2d0bba3f0c6a25532c22224d4
AlmaSnagic <asnagic@hotmail.com>
2025-01-10 20:06:16 +0100

    Urađen zadatak 1

--- vjezba 1/zadatak1.py
+++ vjezba 1/zadatak1.py
@@ -7,14 +7,24 @@ import random
 numbers = []
 
 # TODO: Generišite listu od 10 slučajnih brojeva između 1 i 100
+numbers = [random.randint(1, 100) for _ in range(10)]
+    
 
 # Serijalizacija liste u fajl 'numbers.pkl'
+with open('numbers.pkl', 'wb') as f:
+    pickle.dump(numbers, f)
+
+print("Lista je serijalizovana u fajl 'numbers.pkl'")
 
 # TODO: Serijalizujte listu koristeći pickle i upišite u fajl
 
 # Deserijalizacija i prikaz
 
 # TODO: Učitajte listu iz fajla i ispišite je
+with open('numbers.pkl', 'rb') as file:
+    loaded_numbers = pickle.load(file)
+
+# Ispis učitane lista
 
 print("Originalna lista:", numbers)
 print("Učitana lista:", loaded_numbers)
