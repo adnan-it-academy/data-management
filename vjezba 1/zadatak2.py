9516fc4535a8ae183942d5a2233e5e8202b7c7f9
amx5454 <amarhubjer@gmail.com>
2025-01-10 20:28:55 +0100

    zadatak 2 kraj

--- vjezba 1/zadatak2.py
+++ vjezba 1/zadatak2.py
@@ -17,13 +17,42 @@ users = []
 
 # TODO: Omogućite unos podataka o korisnicima i dodajte ih u listu
 
+def unos_korisnika():
+    username = input("Unesite korisničko ime: ")
+    email = input("Unesite email: ")
+    age = int(input("Unesite godine: "))
+    return User(username, email, age)
+
 # Serijalizacija liste korisnika u fajl 'users.pkl'
+
+def spremi_korisnike(korisnici):
+    with open('users.pkl', 'wb') as file:
+        pickle.dump(korisnici, file)
 
 # TODO: Serijalizujte listu korisnika i sačuvajte u fajl
 
+def ucitaj_korisnike():
+    try:
+        with open('users.pkl', 'rb') as file:
+            return pickle.load(file)
+    except FileNotFoundError:
+        return []
+    
 # Deserijalizacija i ispis korisnika
 
+if __name__ == "__main__":
+    # Unos novih korisnika
+    nastavak = 'da'
+    while nastavak.lower() == 'da':
+        korisnik = unos_korisnika()
+        users.append(korisnik)
+        nastavak = input("Želite li unijeti još korisnika? (da/ne): ")
+
 # TODO: Učitajte korisnike iz fajla i ispišite ih
+    spremi_korisnike(users)
+
+   
+    loaded_users = ucitaj_korisnike()
 
 print("Spremljeni korisnici:")
 for user in loaded_users:
